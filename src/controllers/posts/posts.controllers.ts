dd307b20a8277f8794681a49491096c2458291c0
Linda <lindamaulida1102@gmail.com>
2026-09-17 19:18:40 +0700

    Add category

--- src/controllers/posts/posts.controllers.ts
+++ src/controllers/posts/posts.controllers.ts
@@ -250,8 +250,30 @@ export class PostsController {
     }
   };
 
-  
+  // GET CATEGORIES
+  getCategories = async (req: Request, res: Response) => {
+    try {
+      // Mengambil semua category
+      const categories = await db.select().from(categoryTable);
 
+      return res.status(200).json({
+        success: true,
+        message: "Berhasil mengambil category",
+        data: {
+          categories,
+        },
+      });
+    } catch (error) {
+      console.error("Get categories error:", error);
+
+      return res.status(500).json({
+        success: false,
+        message: "Gagal mengambil category",
+        error: error instanceof Error ? error.message : error,
+      });
+    }
+  };
+  
 }
 
 export default new PostsController();
